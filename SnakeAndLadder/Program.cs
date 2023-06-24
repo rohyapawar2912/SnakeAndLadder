879cc43f2a0b49dde4ddaff617189347b6f5b8cd
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-06-24 12:00:12 +0530

    [Rohit]Add: // add all files

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -10,14 +10,8 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder Game!");
-
-            // Create a new instance of the dice
-            SnakeAndLadderGame snakeandladdergame = new SnakeAndLadderGame();
-
-            // Roll the dice
-            int rollResult = snakeandladdergame.Roll();
-            Console.WriteLine("Dice roll: " + rollResult);
+            SnakeAndLadderGame game = new SnakeAndLadderGame();
+            game.Play();
         }
     }
 }
--- SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -8,16 +8,43 @@ namespace SnakeAndLadder
 {
     public class SnakeAndLadderGame
     {
+        private int currentPlayerPosition;
         private Random random;
 
         public SnakeAndLadderGame()
         {
+            currentPlayerPosition = 0;
             random = new Random();
         }
 
-        public int Roll()
+        public void Play()
         {
-            return random.Next(1, 7);
+            Console.WriteLine("Welcome to Snake and Ladder Game!");
+
+            // Roll the die
+            int dieRoll = random.Next(1, 7);
+            Console.WriteLine("Die roll: " + dieRoll);
+
+            // Determine the player's option (No Play, Ladder, or Snake) using random number generation
+            int option = random.Next(0, 3);
+
+            switch (option)
+            {
+                case 0: // No Play
+                    Console.WriteLine("No Play - Player stays in the same position");
+                    break;
+                case 1: // Ladder
+                    Console.WriteLine("Ladder - Player moves ahead by " + dieRoll + " positions");
+                    currentPlayerPosition += dieRoll;
+                    break;
+                case 2: // Snake
+                    Console.WriteLine("Snake - Player moves behind by " + dieRoll + " positions");
+                    currentPlayerPosition -= dieRoll;
+                    break;
+            }
+
+            // Print the final position of the player
+            Console.WriteLine("Player position: " + currentPlayerPosition);
         }
     }
 }
