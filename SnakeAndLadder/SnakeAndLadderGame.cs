a531d730ea7a66db0d91f50ece883020d48963a0
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-06-24 11:40:22 +0530

    [Rohit]Add: // add all files

---
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class SnakeAndLadderGame
+    {
+        private int currentPlayerPosition;
+
+        public SnakeAndLadderGame()
+        {
+            currentPlayerPosition = 0;
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game!");
+
+            // Simulate the game logic here
+
+            // Print the final position of the player
+            Console.WriteLine("Player position: " + currentPlayerPosition);
+        }    
+    }
+}
